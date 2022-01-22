13eddd632673377cbe6868a1d15f95d5bd7bd26e
Michi Palazzo <palazzomichi@gmail.com>
2022-01-22 19:02:24 +0100

    Update

--- 5 - Generazione del bytecode/Translator.java
+++ 5 - Generazione del bytecode/Translator.java
@@ -178,7 +178,7 @@ public class Translator {
         match('}', "unclosed block '{");
         break;
       default:
-        error("statement expected (assign, print, read, while, if, block)");
+        error("statement expected (assign, print, read, while, if, { statements })");
     }
   }
   
